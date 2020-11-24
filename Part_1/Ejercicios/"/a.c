abfeaa1d351d327425aea6ec5f87963c58de08cc
CesAlexMV <74991728+CesAlexMV@users.noreply.github.com>
2020-11-24 17:19:46 -0600

    Create a.c

---
+++ Part_1/Ejercicios/"/a.c
@@ -0,0 +1,22 @@
+//punto de venta de una ferretería 
+#include <stdio.h>
+#define a=sistema internacional;
+#define b=sistema britanico;
+
+int main(){
+    int a,b 
+    printf("Medida del tornillo", "puede ser a o b");
+    printf("Escribir medida del tornillo );
+    scanf ("La medida es" : f% , a/b)
+    
+     float precio; 
+     float peso; 
+         printf("Escribe una cantidad en pesos:");
+         scanf ("%f",&peso);
+    int 
+    printf("precio x objeto vendido")
+    printf("insertar precio")
+    printf("insertar obtejo vendido")
+
+    return 0;
+}
