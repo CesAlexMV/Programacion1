2f22b1f41ef90e2e9b00e8f4559fed2a3c9d28a4
CesAlexMV <74991728+CesAlexMV@users.noreply.github.com>
2020-11-24 17:25:59 -0600

    Create hermanos_totales.c

---
+++ Part_1/Ejercicios/2/hermanos_totales.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	
+	printf("%cCu%cntos hermanos tienes?\n ",168,160);
+	scanf("%i",&a);
+	b=a+1;
+	printf("\n Son %i hijos en total. (:",b);
+return 0;
+}
