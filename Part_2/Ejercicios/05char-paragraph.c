ab61357fc91659636905a01369e4d5b2c86267e0
CesAlexMV <74991728+CesAlexMV@users.noreply.github.com>
2020-11-25 13:41:07 -0600

    Update 05char-paragraph.c

--- Part_2/Ejercicios/05char-paragraph.c
+++ Part_2/Ejercicios/05char-paragraph.c
@@ -1,12 +1,13 @@
-#include <stdio.h>   
+#include <stdio.h>
+
 int main ( )
 {
-  char n[1];
+  char n[0];
   int a;
-  printf ("Digita una letra.\n");
+  printf ("Digita una letra: \n");
   scanf("%c", &n);
-  printf("Digite un numero");
+  printf("Digite un numero: \n");
   scanf("%i", &a);
-  printf ("\nSu letra es %c y el numero es %i ", n, a);
+  printf ("\nSu letra es %c y el numero es %i ", n[0], a);
   return 0;
 }
