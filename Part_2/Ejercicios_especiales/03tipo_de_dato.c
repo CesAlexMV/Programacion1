18e49685e2d8dffb06cb7f48ed5ee0d0a5033c34
CesAlexMV <74991728+CesAlexMV@users.noreply.github.com>
2020-11-25 13:59:26 -0600

    Create 03tipo_de_dato.c

---
+++ Part_2/Ejercicios_especiales/03tipo_de_dato.c
@@ -0,0 +1,36 @@
+/*Programa que revise si un carácter dados es mayúscula,
+     minúscula, número o un carácter especial.
+             Ejercicios Especiales 2
+                    GRUPO 1151
+                  EQUIPO numero 1                          */
+
+#include <stdio.h> //incluimos librería estándar
+
+int main () //Función principal del programa
+{
+	char i;  //Declaramos una variable tipo carácter
+    
+	printf ("Dame un caracter y te dire lo que es:\n");
+	
+	scanf ("%c",&i); //Asignamos el valor a i
+	
+	if (i>='0' && i<='9') //Si es dígito hará esto
+	{
+		printf("El caracter dado es digito");
+	}
+	else if (i>='a' && i<='z')//Si es minúscula hará esto
+	{
+		printf("El caracter dado es minuscula");
+	}
+	else if (i>='A' && i<='Z')//Si es mayúscula hará esto
+	{
+		printf("El caracter dado es mayuscula");
+	}
+	else
+	{
+		printf("El caracter dado es carcter especial");//Si es caracter especial hará esto
+	}
+	
+	return 0;
+
+}
